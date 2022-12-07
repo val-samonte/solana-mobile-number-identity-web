f7c214c1f459e770558d7d41c85fccdf6ea3af86
Val <val.samonte@gmail.com>
2022-12-08 07:10:49 +0800

    Front page refactor

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,32 +5,36 @@ export function App() {
   const { setVisible } = useWalletModal()
 
   return (
-    <div className='flex flex-col landscape:flex-row m-5 landscape:my-10 gap-5 items-center'>
-      <div className='flex-auto flex flex-col'>
-        <small className='text-base opacity-50'>Mythrilsoft</small>
-        <h1 className='text-4xl font-bold'>identity</h1>
-        <hr className='my-3 opacity-20' />
-        <p className='md:text-lg max-w-sm'>
-          Easily associate your phone number to your Solana wallet address with
-          privacy
-        </p>
-      </div>
-      <div className='flex flex-col gap-5 1flex-none w-full landscape:max-w-sm p-5 bg-zinc-100 text-zinc-900 rounded-xl min-h-[50vh]'>
-        <div className='flex-auto gap-5 flex flex-col items-center justify-center'>
-          <img className='h-64' src='/identity.svg' alt='identity promo art' />
+    <div className='flex flex-col p-5 landscape:py-10 gap-10 items-center justify-center portrait:min-h-screen'>
+      <div className='flex flex-col landscape:flex-row landscape:items-center portrait:gap-5'>
+        <div className='landscape:flex-auto flex items-center justify-center'>
+          <img
+            className='h-64 aspect-square'
+            src='/identity.svg'
+            alt='identity logo'
+          />
         </div>
-        <button
-          type='button'
-          onClick={() => setVisible(true)}
-          className={classNames(
-            'transition-all rounded-full',
-            'p-2 hover:p-3 hover:-m-1',
-            'text-zinc-100 bg-zinc-900 hover:bg-zinc-800',
-          )}
-        >
-          Connect to Get Started
-        </button>
+        <div className='flex-auto flex flex-col'>
+          <small className='text-base opacity-50'>mythrilsoft</small>
+          <h1 className='text-4xl font-bold'>identity</h1>
+          <hr className='my-3 opacity-20' />
+          <p className='md:text-lg max-w-xs'>
+            Easily associate your phone number to your Solana wallet address
+            with privacy
+          </p>
+        </div>
       </div>
+      <button
+        type='button'
+        onClick={() => setVisible(true)}
+        className={classNames(
+          'transition-all rounded-full',
+          'py-3 px-5 hover:py-5 hover:px-7 hover:-m-2',
+          'text-zinc-900 bg-zinc-200 hover:bg-white',
+        )}
+      >
+        Connect to Get Started
+      </button>
     </div>
   )
 }
